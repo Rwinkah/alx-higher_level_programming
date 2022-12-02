55fcb5d480aab30e3fd3d38349b4ada29269ce79
Rwinkah <rwinosa@gmail.com>
2022-12-02 04:17:37 +0100

    fix import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-c = add(a, b)
-print("{} + {} = {}".format(a, b, c))
+def main():
+    """ Import add function from module
+    and ensure the  entire module does not run when imported
+    """
+    from add_0 import add
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
+
+
+if __name__ == '__main__':
+    main()
