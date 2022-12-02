d5c9a509cfe9e37824cc2f53fdd460b5baab7139
Rwinkah <rwinosa@gmail.com>
2022-12-02 05:27:42 +0100

    fix: import error

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def main():
+    import hidden_4
     """ Display all names defined in a module"""
-    for name in dir('hidden-4.pyc'):
+    for name in dir(hidden_4):
         if name[0] != '_' and name[1] != '_':
             print(name)
 
