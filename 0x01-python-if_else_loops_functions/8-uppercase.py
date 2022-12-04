332ea7dc418b07c4c18fa1999cea5f413678cd37
Rwinkah <rwinosa@gmail.com>
2022-12-05 00:16:19 +0100

    fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,14 +2,10 @@
 def uppercase(str):
     if not str:
         return
-    for i in range(len(str) - 1):
+    for i in range(len(str)):
         if ord(str[i]) in range(97, 123):
             msg = chr(ord(str[i]) - 32)
         else:
             msg = str[i]
         print('{}'.format(msg), end='')
-    if ord(str[-1]) in range(97, 123):
-        msg = chr(ord(str[-1]) - 32)
-    else:
-        msg = str[-1]
-    print('{}'.format(msg))
+    print("")
