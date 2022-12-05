871ccc9114d291f8373159724e5b6b20170d7936
Rwinkah <rwinosa@gmail.com>
2022-12-06 00:00:23 +0100

    fix: return

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if not my_list:
+        return None
     if idx < len(my_list) and idx > 0:
         my_list[idx] = element
     return my_list
