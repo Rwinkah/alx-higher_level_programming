3df8dd7339ae2d1eff40e716742a93ef1e1ca48c
Rwinkah <rwinosa@gmail.com>
2022-12-07 15:48:35 +0100

    task 6 0x03

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    i = 1
+    for row in matrix:
+        for cell in row:
+            print('{}'.format(cell), end='')
+            i += 1
+            if i <= len(row):
+                print(', ', end="")
+            else:
+                print("")
+        i = 1
