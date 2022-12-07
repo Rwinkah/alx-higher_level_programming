8484add5b8467a34073a422c7e5c27d46c7cf639
Rwinkah <rwinosa@gmail.com>
2022-12-07 15:59:40 +0100

    fix: null matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     i = 1
+    if matrix is None:
+        return
     for row in matrix:
         for cell in row:
             print('{:d}'.format(cell), end='')
