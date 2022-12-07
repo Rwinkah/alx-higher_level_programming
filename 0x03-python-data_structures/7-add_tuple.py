cf399f8acb1ade0c5be5aea322176e19a4c34e82
Rwinkah <rwinosa@gmail.com>
2022-12-07 16:41:26 +0100

    fix: index error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     while len(list_b) < 2:
         list_b.append(0)
     tuple_b = list(list_b)
+    list_a = list(tuple_a)
+    while len(list_a) < 2:
+        list_a.append(0)
+    tuple_a = list(list_a)
     zip_list = list(zip(tuple_a, tuple_b))
     i = 0
     list_ans = []
