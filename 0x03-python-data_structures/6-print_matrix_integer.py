264d5f932ebc7fc35db1448cce2026b6995ba0cd
Rwinkah <rwinosa@gmail.com>
2022-12-07 16:12:05 +0100

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     i = 1
-    if matrix is None:
-        print("matrix is none")
+    if matrix == [[]]:
+        print("")
         return matrix
     for row in matrix:
         for cell in row:
