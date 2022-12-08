498bf250036984add692ce6028abe9a71d9f26f4
Rwinkah <rwinosa@gmail.com>
2022-12-08 12:58:54 +0100

    task 1 0x04

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list == []:
+        return my_list
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == int(search):
+            new_list[i] = int(replace) 
+    return new_list
