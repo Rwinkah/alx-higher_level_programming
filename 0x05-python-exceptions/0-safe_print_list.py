4cd1bb021c03b9c66b4ca6491ca557d41644cc46
Rwinkah <rwinosa@gmail.com>
2022-12-21 19:48:51 +0100

    fix: wrong output

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,6 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    if (x == 0 or my_list == []):
-        return (0)
     for num in range(x):
         try:
                 print(my_list[num], end='')
