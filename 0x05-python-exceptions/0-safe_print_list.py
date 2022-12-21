b2a455cadf7b26f93954c2a514ed1b56d4e8b524
Rwinkah <rwinosa@gmail.com>
2022-12-21 14:33:15 +0100

    fix handler

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,7 +10,6 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print(my_list[num], end='')
                 i += 1
-        except:
-            return(i)
+        except (IndexError):
+            return (i)
     return (i)
-    
