0a07a31177a0241c68cbec1b23b1491caaae8cc1
Rwinkah <rwinosa@gmail.com>
2022-12-21 14:26:06 +0100

    task 0 0x05

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for num in range(x):
+        try:
+            if (num == (x - 1) or num == x):
+                print(my_list[num])
+                i += 1
+            else:
+                print(my_list[num], end='')
+                i += 1
+        except:
+            return(i)
+    return (i)
+    
