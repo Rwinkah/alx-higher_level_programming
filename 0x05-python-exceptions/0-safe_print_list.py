2c78e148e386f206264111e0391d1cdf9dad2f62
Rwinkah <rwinosa@gmail.com>
2022-12-21 19:46:47 +0100

    fix: index error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,13 +6,11 @@ def safe_print_list(my_list=[], x=0):
         return (0)
     for num in range(x):
         try:
-            if (num == (x - 1) or num == x):
-                print(my_list[num])
-                i += 1
-            else:
                 print(my_list[num], end='')
                 i += 1
         except (IndexError):
-            print("")
-            return (i)
+            break
+
+    print("")
     return (i)
+	
