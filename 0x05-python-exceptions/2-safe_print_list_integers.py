607bce3fde2d3d5093703d09a198ff0dca42ff1a
Rwinkah <rwinosa@gmail.com>
2022-12-21 20:19:14 +0100

    pycodestyle fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[num]), end='')
             i += 1
         except (ValueError, TypeError):
-           continue
+            continue
 
     print("")
     return (i)
