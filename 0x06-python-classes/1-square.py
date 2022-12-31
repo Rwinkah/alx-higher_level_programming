98f477833c37df50cff46379c4481acf63415fdd
Rwinkah <rwinosa@gmail.com>
2022-12-31 09:58:38 +0100

    fix: doc error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+""" Module defining a class square """
+
 
 class Square:
     def __init__(self, size):
+        """
+            Initialization method for the class
+            Args:
+               size(int): Size of the square.
+        """
         self.__size = size
