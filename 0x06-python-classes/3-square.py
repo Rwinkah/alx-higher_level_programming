2219d76a23a670cc737dbcfab9996defda3aacb3
Rwinkah <rwinosa@gmail.com>
2022-12-31 10:25:48 +0100

    task 3 0x06

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Module defining a class square with validation for size """
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        """ Initialization method for the class
+        Args:
+                size(int): Size of the square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
