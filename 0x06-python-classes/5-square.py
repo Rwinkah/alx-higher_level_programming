c418fc22d7f61cf494f22b5481ea2fd8c38ee4c5
Rwinkah <rwinosa@gmail.com>
2023-01-01 01:59:01 +0100

    task 5 0x06

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Square class with private property implementing getters and setters """
+
+
+class Square:
+    """ A class to show the getter encapsulation of private attributes"""
+
+    def __init__(self, sizee=0):
+        self.__size = sizee
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, sizee):
+        if type(sizee) != int:
+            raise TypeError("size must be an integer")
+        elif sizee < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = sizee
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        area = self.__size
+        for length in range(area):
+            for breadth in range(area):
+                print('#', end='')
+            print("")
