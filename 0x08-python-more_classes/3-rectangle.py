abe158968a6e267da71b65805b8c4021b64d0710
Rwinkah <rwinosa@gmail.com>
2023-01-03 00:07:12 +0100

    task 3 0x08

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+""" Module defining a class Rectangle """
+
+
+class Rectangle:
+    """ Proper Rectangle class """
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """ get the height of the rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ Sets the height of the rectangle """
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        """ gets the width of the rectangle """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ set the width of the rectangle """
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+    def area(self):
+        """ Returns the are of the rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Returns the perimeter of the rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        disp = []
+        for i in range(self.__height):
+            for width in range(self.__width):
+                disp.append("#")
+            if i != self.height - 1:
+                disp.append("\n")
+ 
+        return "".join(disp)
