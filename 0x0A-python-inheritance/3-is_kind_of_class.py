522276e23079edba38e5ddf0458bc719af59b543
Rwinkah <rwinosa@gmail.com>
2023-02-07 12:12:03 +0100

    3 task 0xx0A

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Check if Obj is an instance of a class """
+
+
+def is_same_class(obj, a_class):
+    """ function to perform the check"""
+    return (isinstance(obj, a_class))
