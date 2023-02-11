20d63e2fdd545217be48d7530e2687a43cc33d17
Rwinkah <rwinosa@gmail.com>
2023-02-11 15:58:57 +0100

    fixagain

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module creating a class named Rectangle """
+"""creating a class named Rectangle that inherits from BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
