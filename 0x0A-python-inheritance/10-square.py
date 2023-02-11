418a34e74fc4ac0b49b6b719673ea2c86424ee4b
Rwinkah <rwinosa@gmail.com>
2023-02-11 17:25:50 +0100

    fix: syntax error

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -11,9 +11,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Initialize Square class."""
-        self.integervalidator("size", size)
+        self.integer_validator("size", size)
         self.__size = size
-    
+
     def area(self):
         """Return area of the square."""
         return self.__size * self.__size
