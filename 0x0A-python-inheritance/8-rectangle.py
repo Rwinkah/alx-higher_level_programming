a7858f9a1cb2769e426cbfced5e4007df13da74b
Rwinkah <rwinosa@gmail.com>
2023-02-11 15:17:49 +0100

    8 task 0x0a

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module creating an empty class """
+
+
+class Rectangle(BaseGeometry):
+    """An subclass of BaseGeometry named Rectangle"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self):
+        if type(self.__height) != int:
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be greater than 0")
+
+        if type(self.__width) != int:
+            raise TypeError(" width must be an integer")
+        if self.__height <= 0:
+            raise ValueError("width must be greater than 0")
