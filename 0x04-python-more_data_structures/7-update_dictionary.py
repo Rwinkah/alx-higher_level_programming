0be6055290423f0cd2281ebcdb77ed285e048ac5
Rwinkah <rwinosa@gmail.com>
2023-02-12 10:27:51 +0100

    7 task 0x04

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define function to update dictionary values"""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Updating dictionary values
+    Args:
+        a_dictionary(dict): dictionary to update
+        key(string): Key to add or update in dictionary
+        value(any): value to add with key
+    """
+    a_dictionary[key] = value
