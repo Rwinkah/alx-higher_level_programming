7b67c9f62fba3e2cf77bd56e60a12c816061a1e7
Rwinkah <rwinosa@gmail.com>
2023-02-12 10:12:35 +0100

    fix: naming error

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,16 @@
+!/usr/bin/python3
+"""Defines function for printing dictionary elements by sorted keys."""
+
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    Print dictionary by sorted keys
+    Args:
+        a_dictionary(dict): dictionary to sort
+    """
+
+    list_keys = list(a_dictionary.keys())
+    list_keys.sort()
+
+    for i in list_keys:
+        print("{}: {}".format(i, a_dictionary[i]))
