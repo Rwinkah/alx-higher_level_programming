2fe2a2a451daff469dae5d992b0557d589446cdc
Rwinkah <rwinosa@gmail.com>
2023-03-04 23:50:55 +0100

    fix: task 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining a function to delete a member of a dictionary using a key."""
+
+
+def simple_delete(a_dictionary, key=""):
+    """
+    simple delete from dictionary
+    Args:
+        a_dictionary(dict): dictionary to delete from
+        key(string): key to delete from dict
+    """
+    if key not in a_dictionary:
+        return
+
+    a_dictionary.pop(key)
+    return a_dictionary
