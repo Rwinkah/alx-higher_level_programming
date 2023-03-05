623dc83f7758388c991fffec8423527677ab551f
Rwinkah <rwinosa@gmail.com>
2023-03-05 16:41:03 +0100

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman_dict = {'I':1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if type(roman_string) != str or roman_string is None:
+        return 0
+    rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     list_roman = list(roman_string)
     sum_num = 0
 
     for i in range(len(list_roman)):
         if i == 0:
-            sum_num += roman_dict[list_roman[i]]
-        elif roman_dict[list_roman[i]] > roman_dict[list_roman[i-1]]:
-            sum_num -= 2*roman_dict[list_roman[i-1]]
-            sum_num += roman_dict[list_roman[i]]
+            sum_num += rom_dict[list_roman[i]]
+        elif rom_dict[list_roman[i]] > rom_dict[list_roman[i-1]]:
+            sum_num -= 2*rom_dict[list_roman[i-1]]
+            sum_num += rom_dict[list_roman[i]]
         else:
-            sum_num += roman_dict[list_roman[i]]
+            sum_num += rom_dict[list_roman[i]]
     return sum_num
-            
