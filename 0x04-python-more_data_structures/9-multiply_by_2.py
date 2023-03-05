8ed512de7deb84fd607a158c5345c1fbedd63026
Rwinkah <rwinosa@gmail.com>
2023-03-05 15:14:46 +0100

    9 task 0x04

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,16 @@
-
 #!/usr/bin/python3
 
-def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    listkey = list(new_dict.keys())
+def multiply_by_2(inp_dict):
+    """
+    Function returns a new dictionary with all integer values multiplied by 2
+    Args:
+        a_dictionary: input dictionary
+    Return:
+        new_dictionary
+    """
 
-   for i in listkey:
+    list_keys = [i for i in inp_dic.keys()]
+    new_dict = inp_dict.copy()
+    for i in list_keys:
        new_dict[i] *= 2
-
-   return new_dict 
+    return new_dict
