0161e992d39e1d5c37ba42f35557eff5842f23d9
Rwinkah <rwinosa@gmail.com>
2023-03-05 22:23:23 +0100

    102 task 0x04

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    """
-    square all values in a matrix
-    Args:
-        matrix = input matrix
-    Return:
-        new_matrix
-    """
     return(list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
