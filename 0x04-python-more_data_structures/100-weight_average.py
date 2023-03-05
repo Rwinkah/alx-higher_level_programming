c532dbd5edf09e43c5422ed64b99267ea5548fd1
Rwinkah <rwinosa@gmail.com>
2023-03-05 18:48:04 +0100

    task 100 0x04

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    """
+    return weighted average from a list of tuples
+    Args:
+        my_list: input list
+    Return:
+        weighted average: float
+    """
+    if len(my_list) == 0 or my_list is None:
+        return 0
+
+    we_average = 0
+    we_div = 0
+
+    for i in my_list:
+        we_average += i[0] * i[1]
+        we_div += i[1]
+    
+    return we_average / we_div 
+
+
