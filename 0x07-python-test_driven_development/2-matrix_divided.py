7f78a45eda3f2840605ebe1b351b136fa8e28939
Rwinkah <rwinosa@gmail.com>
2023-03-10 17:47:43 +0100

    3 task 0x07

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,19 +14,20 @@ def matrix_divided(matrix, div):
     function to perform scalar division on a matrix
     """
 
+    not_mat = r'matrix must be a matrix (list of lists) of integers/floats'
     if (not isinstance(div, int) and not isinstance(div, float)):
         raise TypeError("div must be a number")
     if not isinstance(matrix, list):
-        raise TypeError("matrix musr be a matrix (list of lists) of integers/floats")
+        raise TypeError(not_mat)
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     for lst in matrix:
         if not isinstance(lst, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(not_mat)
         for num in lst:
             if not isinstance(num, int) and not isinstance(num, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(not_mat)
     lenlist = len(matrix[0])
     for lst in matrix:
         if len(lst) != lenlist:
