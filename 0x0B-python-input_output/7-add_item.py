bb0c054539486adff603c3e2f24f6cf80297c1b2
Rwinkah <rwinosa@gmail.com>
2023-03-13 09:38:01 +0100

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-This module adds all args to python list
-and then saves them to a file
-"""
+""" This module adds all args to python list
+and then saves them to a file """
 import sys
-load_js = __import__('6-load_from_json_file').load_from_json_file
-save_js = __import__('5-dave_to_json_file').save_to_json_file
+load_from_json = __import__('6-load_from_json_file').load_from_json_file
+save_to_json = __import__('5-save_to_json_file').save_to_json_file
 
 
 try:
-    items = load_js('add_item.json')
+    items = load_from_json('add_item.json')
 except FileNotFoundError:
     items = []
 
 items.extend(sys.argv[1:])
-save_js(items, 'add_item.json')
+save_to_json(items, 'add_item.json')
