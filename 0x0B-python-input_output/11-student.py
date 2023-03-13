208030f6954e466d92460b87406ae5b4d740a527
Rwinkah <rwinosa@gmail.com>
2023-03-13 09:00:23 +0100

    11-task[B

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Update on class student """
+
+
+class Student:
+    """ Class for student """
+    def __init__(self, first_name, last_name, age):
+        """ init function for student """
+        self.fname = first_name
+        self.lname = last_name
+        self.age = age
+
+    def to_json(self, att=None):
+        """ return dict representation of required attributes """
+
+        if (isinstance(att, list) and all(isinstance(i, str) for i in att)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json_dict):
+        """ load data for attrs from student """
+        for attrib, value in json_dict.items():
+            setattr(self, attrib, value)
