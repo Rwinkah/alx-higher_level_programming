346232eb0712e8683e47fef76098c1fc4d46a667
Rwinkah <rwinosa@gmail.com>
2023-03-13 09:33:27 +0100

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" This module adds all args to python list and then saves them to a file """Z
-
-
+"""
+This module adds all args to python list and then saves them to a file
+"""
 import sys
-
 load_js = __import__('6-load_from_json_file').load_from_json_file
 save_js = __import__('5-dave_to_json_file').save_to_json_file
 
