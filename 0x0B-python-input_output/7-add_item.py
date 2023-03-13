3ab5aabc08a9ae7895311f9b6ad0efa769debca7
Rwinkah <rwinosa@gmail.com>
2023-03-13 08:29:16 +0100

    9 task 0x0B

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This module adds all args to python list and then saves them to a file """
+import sys
+
+load_js = __import__('6-load_from_json_file').load_from_json_file
+save_js = __import__('5-dave_to_json_file').save_to_json_file
+
+
+try:
+    items = load_js('add_item.json')
+except FileNotFoundError:
+    items = []
+items.extend(sys.argv[1:])
+save_js(items, 'add_item.json')
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module returns the dictionary description with simple 
+    data structure for json serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """ Return dictionary description """
+    return obj.__dict__
