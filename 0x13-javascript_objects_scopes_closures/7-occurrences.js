301cee836d8ca88bcabbdd277de5c4b6d29da57e
Rwinkah <rwinosa@gmail.com>
2023-03-15 05:24:11 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.nbOccurrences = (list, searchElement) => {
+exports.nbOccurences = (list, searchElement) => {
   const len = list.length;
   let count = 0;
   for (let i = 0; i < len; i++) {
