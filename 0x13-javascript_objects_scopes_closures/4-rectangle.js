ba31af468304f540c744052ff34c0042cacfb6e3
Rwinkah <rwinosa@gmail.com>
2023-03-15 03:59:50 +0100

    4 task 0x13

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((typeof w === 'number' && w > 0) && (typeof h === 'number' && h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let j = 0; j < this.width; j++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
