2031de3ea8f109002baf595e7e9257749e19dde9
Rwinkah <rwinosa@gmail.com>
2023-03-16 23:06:46 +0100

    0 task 0x0F

---
+++ 0x0F-python_object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" mysqldb project, intro to object relational mappers """
+
+
+def main():
+
+    import argparse
+    import MySQLdb
+    parser = argparse.ArgumentParser(description='[username] [password] [database]')
+
+    parser.add_argument('uname', help="your sql username")
+    parser.add_argument('passwd', help="your sql password")
+    parser.add_argument('dbase', help="your database name")
+
+    args = parser.parse_args()
+
+    db = MySQLdb.connect(host='localhost', user=args.uname, passwd=args.passwd, port=3306, db=args.dbase)
+
+    curs = db.cursor()
+
+    curs.execute("SELECT * FROM states ORDER BY id")
+    records = curs.fetchall()
+    for record in records:
+        print(record)
+    curs.close()
+    db.close()
+
+if __name__ == "__main__":
+    main() 
