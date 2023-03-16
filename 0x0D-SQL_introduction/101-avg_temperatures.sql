02831fd3caca64a1d6ed5e837ec559302878f674
Rwinkah <rwinosa@gmail.com>
2023-03-16 13:23:47 +0100

    fix

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- calc average temperature and sort by avg temp
-SELECT city, AVG(value) AS avg_temp FROM temperatures ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
