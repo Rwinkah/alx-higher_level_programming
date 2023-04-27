eec286ea1432c83ebbd28c0068e8df364584ee9d
Rwinkah <rwinosa@gmail.com>
2023-04-27 10:54:14 +0100

    3 task 0x14

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const response = request.get('https://swapi-api.alx-tools.com/api/films/1', (error, response, body) => {
+  console.log(response.statusCode)
+  if (error) {
+    console.log(error)
+  }
+  else if (response.statusCode == 200) {
+    const respInfo = JSON.parse(body)
+    console.log(respInfo.title)
+  }
+})
