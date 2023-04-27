6ad55f8d0f37f2114624e8a8da5595d7463ce93e
Rwinkah <rwinosa@gmail.com>
2023-04-27 10:57:59 +0100

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,6 @@
 
 const request = require('request');
 const response = request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (error, response, body) => {
-  console.log(response.statusCode)
   if (error) {
     console.log(error)
   }
