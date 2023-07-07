c117ec8166254c77df32483f35aa43debec3557e
rwinkah <rwinosa@gmail.com>
2023-07-07 06:09:15 +0100

    100 task 0x11

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python script to access github api and print latest commits
+"""
+
+from sys import argv
+import requests
+
+
+def main():
+    """
+    main function of github scraper
+    """
+
+    repo = argv[1]
+    owner = argv[2]
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+    resp = requests.get(url).json()
+
+    for commit in resp[:10]:
+        out = f"{commit['sha']} : {commit['author']['login']}"
+        print(out)
+
+
+if __name__ == '__main__':
+    main()
