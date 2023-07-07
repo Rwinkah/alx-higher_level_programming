bb15e6c747043e2b40df7928d2a8dbb06fde16b8
rwinkah <rwinosa@gmail.com>
2023-07-07 08:09:27 +0100

    4 task 0x11

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script using request to hit a url
+"""
+
+
+def main():
+    """
+    Wrapped in main to prevent run on import
+    """
+    import requests as req
+    r = req.get("https://alx-intranet.hbtn.io/status")
+    print('Body response:')
+    print(f'\t- type: {type(r.content)}')
+    print(f'\t- content: {r.text}')
+
+
+if __name__ == '__main__':
+    main()
