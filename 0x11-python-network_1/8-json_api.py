1ec534e0b93f4acd8fcd8de03db3cb7cb14c2cca
rwinkah <rwinosa@gmail.com>
2023-07-09 20:58:39 +0100

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -24,7 +24,7 @@ def main():
             print('No result')
         else:
             print(f"[{jsN['id']}] {jsN['name']}")
-    except json.Decoder.JSONDecodeError as e:
+    except json.decoder.JSONDecodeError as e:
         print('Not a valid JSON')
 
 
