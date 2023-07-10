c7823e0ac7fbcd1adb2a26d5eaea32f2310d4f16
rwinkah <rwinosa@gmail.com>
2023-07-10 01:41:55 +0100

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,5 +5,29 @@ my_github
 Module to access github api and return id number.
 """
 
-import requests
-import sys
+
+def main():
+    """
+    Code wrapped to prevent run on import.
+    """
+    import requests as req
+    from sys import argv
+    from requests.auth import HTTPBasicAuth as httpAuth
+
+    username = argv[1]
+    password = argv[2]
+    authe = httpAuth(username, password)
+
+    r = req.get('https://api.github.com/users', auth=authe)
+    if r.status_code == 200:
+        try:
+            response = r.json()
+            print(response[id])
+        except ValueError:
+            print('None')
+    else:
+        print('None')
+
+
+if __name__ == '__main__':
+    main()
