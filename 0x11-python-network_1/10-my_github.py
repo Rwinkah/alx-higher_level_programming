c2086b23edd852294ea3b58faa20a9f3a6f5d4e1
rwinkah <rwinosa@gmail.com>
2023-07-10 01:34:51 +0100

    fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,34 +1,6 @@
 #!/usr/bin/python3
 """
-Module to check for git id
+my_github
+
+Module to access github api and return id number.
 """
-
-
-def main():
-    """
-    Code wrapped to prevent run on import
-    """
-    from sys import argv
-    import requests as req
-    from requests.auth import HTTPBasicAuth as httpAuth
-
-    password = argv[2]
-    username = argv[1]
-
-    auth = httpAuth(username, password)
-
-    r = req.post('https://api.github.com/users', auth=auth)
-    if r.status_code == 200:
-        try:
-            resp = r.json()
-            print(r.json()['id'])
-        except ValueError:
-            print('None')
-    else:
-        print('None')
-
-
-if __name__ == '__main__':
-    main()
-
-print(self.__doc__)
