e937dc6b2110eaf40b12cdf47743794e38083d23
rwinkah <rwinosa@gmail.com>
2023-07-14 10:25:49 +0100

    fix: syntax

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Selecting only content length header from curl response
 
- curl -sI localhost | grep Content-Length: | grep -o '[0-9]\+'
+curl -sI localhost | grep Content-Length: | awk '{print $2}'
