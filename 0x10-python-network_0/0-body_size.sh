88b76e63b5ee0a2085c4a71185e9bae6e2584eb9
rwinkah <rwinosa@gmail.com>
2023-07-14 10:30:56 +0100

    fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Selecting only content length header from curl response
-curl -sI localhost | grep Content-Length: | awk '{print $2}'
+curl -sI $1 | grep Content-Length: | awk '{print $2}'
