af388e828db01ed064354aed5ee82589b15cf4b0
rwinkah <rwinosa@gmail.com>
2023-07-27 03:32:29 +0100

    0 task 0x14

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* Script to read and print content of a file */
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
