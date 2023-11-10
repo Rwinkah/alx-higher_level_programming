c49cbcc0f51bc4139194cc92b80e9bf5cfe3058e
rwinkah <rwinosa@gmail.com>
2023-11-10 04:12:18 +0100

    fix style errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""  python function to add two numbers and test the function using doctest module
+"""  python function to add two numbers and
+test the function using doctest module
 """
 
-def add_integer(a, b=98): 
-	""" 
-	function to add two numbers 
-	args:
-		a: int / float
-		b: int / float
-	return:
-		int
+
+def add_integer(a, b=98):
+    """
+    function to add two numbers
+        args:
+            a: int / float
+            b: int / float
+        return:
+            int
 """
 
-	if type(a) != float and type(a) != int:
-		raise TypeError("a must be an integer")
-	if type(b) != float and type(b) != int:
-		raise TypeError("b must be an integer")
-	return(int(a) + int(b))
+    if type(a) != float and type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != float and type(b) != int:
+        raise TypeError("b must be an integer")
+    if a == NaN or b == NaN:
+        raise TypeError('cannot convert float NaN to integer')
+    return(int(a) + int(b))
